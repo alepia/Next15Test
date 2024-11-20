75e59a5e66c659d55e85792f6d6c27b14d10bd57
alepia <alipi92@gmail.com>
2024-11-20 14:34:09 +0100

    delete

--- components/navbar/Navbar.tsx
+++ components/navbar/Navbar.tsx
@@ -6,7 +6,6 @@ const navItems = [
     {path: '/about', text: 'About'},
     {path: '/contact', text: 'Contact'},
     {path: '/pricing', text: 'Pricing'},
-    {path: '/prueba', text: 'Prueba'},
 ]
 
 export const Navbar = () => {
